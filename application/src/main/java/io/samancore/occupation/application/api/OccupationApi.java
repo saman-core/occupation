65cd9e3e7b973f9519488e6af64b7c83abc8a48f
Hernael <hperer@movealong.tech>
2024-05-08 18:30:20 -0400

    add oidc

--- application/src/main/java/io/samancore/occupation/application/api/OccupationApi.java
+++ application/src/main/java/io/samancore/occupation/application/api/OccupationApi.java
@@ -4,6 +4,7 @@ import io.samancore.occupation.application.service.OccupationService;
 import io.samancore.occupation.data.utils.page.PageUtil;
 import io.samancore.occupation.model.Occupation;
 import io.samancore.occupation.model.PageData;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -26,6 +27,7 @@ public class OccupationApi {
 
     @GET
     @Path("/")
+    @RolesAllowed({"admin"})
     public PageData<Occupation> getAll(@RestQuery("label__regex") String searchValue) {
         log.debug("OccupationApi.getAll");
         var pageRequest = PageUtil.getPage(uriInfo.getQueryParameters());
